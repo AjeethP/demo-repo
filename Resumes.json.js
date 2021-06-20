5c95f82dde912b238fe3249bfd02a36478b35052
AjeethP <85919807+AjeethP@users.noreply.github.com>
2021-06-20 11:57:48 +0530

    resume.md

---
+++ Resumes.json.js
@@ -0,0 +1,34 @@
+var resume=[{
+    "PROFILE":
+{"Name":"Ajeeth P","Department":"Electrical and Electronics Engineering",
+"Education":[{"year":2020,"Qualify":"B.E-EEE","Institute":"SRMValliammai","Percentage":70},
+{"year":2016,"Qualify":"HSC","Institute":"St.Gabriel's","Percantage":88.9},
+{"year":2014,"Qualify":"SSLC","Instiute":"St.Gabriel's","Percentage":95.6},],
+},
+"CAREER OBJECTIVE":
+"Seeking a career that is more innovative and interesting and a job that gives me a oppertunities to learn and enhance my skills with company goals and objectives",
+"PROJECT DETAILS":
+{
+    "TITLE":"SMS Based Accident Alert With GPS CoOrdinates",
+    "OBJECTIVE":"The main aim of this project is to detect te accident location by GPS and send message to inform friends and relatives"},
+"TECHNICAL SKILLS":
+{"IT":["c","c++","JAVASCRIPT"],"EEE":["MATLAB","ETAP"]},
+"AREA OF INTEREST":
+["Circuit designing","Power transmission and distribution","Power system Analysis"],
+"CO-CURRICULAR ACTIVITIES":
+{"INTERNSHIP":["Corporate training","Workshop on Android","Embedded Systems"],
+"VALUE ADDED COURSE":["Modelling and Application of Electrical systems using ETAP and MATLAB","Application on Control systems"]},
+"EXTRA CURRICULAR ACTIVITIES":
+["Organised and CoOrdinated National level Techincal symposium at Valliamai Engineering College",
+"Trained 20 students for shuttle cock zonal selection"],
+"ACHIVEMENTS":
+["Secure Second Highest mark on HSC in School level",
+"Played shuttle cock in zonal selection ",
+"obtained second prize in college level project expo"],
+"CONTACT ME AT":
+[{"Address":"No.68 Gandhipuram East,Pettavaithalai,Trichy(DT).PIN:639112"},
+{"PnoneNo":"9600812989"},
+{"EmailId":"ajeeth301998@gmail.com"}]
+
+}]
+console.log(resume)
